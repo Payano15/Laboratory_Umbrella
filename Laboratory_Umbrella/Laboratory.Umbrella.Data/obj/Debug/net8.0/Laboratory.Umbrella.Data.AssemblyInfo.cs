74f930a7749eb44ce0f4f9e3a4a683f4f9e623b9
jesus manuel alcantara payano <manuel_payano15@hotmail.com>
2026-01-23 23:57:02 -0400

    update

--- Laboratory_Umbrella/Laboratory.Umbrella.Data/obj/Debug/net8.0/Laboratory.Umbrella.Data.AssemblyInfo.cs
+++ Laboratory_Umbrella/Laboratory.Umbrella.Data/obj/Debug/net8.0/Laboratory.Umbrella.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratory.Umbrella.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9dc43c14b25488a1d9c080a9d1cb721744314e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de086ad9fa3b68bd43118fa184a3ac778bfe612d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratory.Umbrella.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratory.Umbrella.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
